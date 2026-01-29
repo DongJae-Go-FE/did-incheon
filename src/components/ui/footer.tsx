cecad42048dedb0dfb22fdd158581332e66a19b4
가동재 <masterforce999@naver.com>
2026-01-29 20:44:13 +0900

    업데이트

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -22,9 +22,11 @@ export default function Footer() {
         </h3>
         <address className="not-italic">
           <ul className="flex flex-col gap-y-2 body02R">
-            <li>도로명 주소 : 인천광역시 동구 박문로 1 인천교구청 102호 WYD 인천 교구대회 사무국</li>
-            <li>지번 주소 : 인천광역시 동구 송림동 103-25</li>
-            <li>이메일 : masterforce999@naver.com</li>
+            <li>
+              도로명 주소 : 인천광역시 동구 박문로 1 인천교구청 102호 WYD 인천
+              교구대회 사무국
+            </li>
+            <li>이메일 : qna.wyd.doc.icn@gmail.com</li>
           </ul>
         </address>
       </div>
